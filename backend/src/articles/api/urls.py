7a1a319b0f156b07d215bbbec77a2b5809d84ec7
Alexnader Watermen <AlexanderWatermenAWC@gmail.com>
2020-04-05 18:02:16 -0400

    fixing histories

--- backend/src/articles/api/urls.py
+++ backend/src/articles/api/urls.py
@@ -4,5 +4,5 @@ from .views import AricalsDetailView, AricalsListView
 
 urlpatterns = [
     path('', AricalsListView.as_view()),
-    path('<pk>', AricalsDetailView.as_view())
+    path('<pk>', AricalsDetailView.as_view()),
 ]
