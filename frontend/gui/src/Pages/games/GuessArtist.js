51f3da44addb703240dd758ea9e2a708f4803f12
Lenora Batcher <lenora.batcher@mymail.champlain.edu>
2020-04-25 15:33:25 -0400

    made a guess the artist game, pull from spotify

--- frontend/gui/src/Pages/games/GuessArtist.js
+++ frontend/gui/src/Pages/games/GuessArtist.js
@@ -1,4 +1,4 @@
-import React from 'react';
+/*import React from 'react';
 
 function GuessA() {
     return (
@@ -6,3 +6,173 @@ function GuessA() {
     )
 }
 export default GuessA
+
+import React from 'react'
+import './tic_tac_toe.css'
+import SpotifyClient from './SpotifyClient';
+*/
+
+//react component to create board, also has function to clear board
+
+import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import SpotifyClient from '../../SpotifyClient.js';
+import './GuessArtist.css'
+
+
+var trackID = "3n3Ppam7vgaVa1iaRUc9Lp";
+var answered = false;
+var artistsArr = [];
+var showImage = ""
+
+function getSongData(result) {
+    var song = result.name;//.data.album.name;
+    var images = result.album.images;
+    var image;
+    if (images.length > 0){
+        image = images[0];
+    } else {
+        image = "https://us.123rf.com/450wm/pavelstasevich/pavelstasevich1811/pavelstasevich181101065/112815953-stock-vector-no-image-available-icon-flat-vector.jpg?ver=6";
+    }
+
+    var artists = (result.album.artists);
+    artistsArr = [];
+    for (var i = 0; i < artists.length; i++){
+        artistsArr.push(artists[i].name.toUpperCase());
+    }
+    console.log(artistsArr);
+    console.log(image);
+
+    showImage = "<img class ='cover' src='" + image.url + "' alt='album cover'>";
+    document.getElementById("title").innerHTML = song;
+
+}
+
+function failure(error) {
+    console.error("An error occurred");
+}
+
+function printArtists(){
+    var artistsStr = ""
+    for (var i = 0; i < artistsArr.length; i++){
+        artistsStr += artistsArr[i] + " ";
+    }
+    return artistsStr;
+}
+
+function showAnswer(correct){
+    document.getElementById("albumCover").innerHTML = showImage;
+    var result = document.getElementById("result");
+    if (answered){
+        result.innerHTML = "You already answered for this question, please generate a new song";
+        result.classList.remove("correct");
+        result.classList.add("incorrect");
+    } else {
+        if (correct){
+            result.innerHTML = "Correct<br />";
+            result.classList.remove("incorrect");
+            result.classList.add("correct");
+        } else {
+            result.innerHTML = "Incorrect<br />";
+            result.classList.remove("correct");
+            result.classList.add("incorrect");
+        }
+        result.innerHTML += "Correct answer(s):<br />" + printArtists();
+        answered = true;
+    }
+}
+
+function playlistsSuccess(response){
+    var playlists = response.data.playlists.items;
+    var num = Math.floor(Math.random() * Math.floor(playlists.length));
+    var playlist = playlists[num];
+
+    var songs = SpotifyClient.getSongsOnPlaylist(playlist.id);
+    songs.then(songsSuccess, failure)
+}
+
+function songsSuccess(response){
+    var songs = response.data.items;
+    var num = Math.floor(Math.random() * Math.floor(songs.length));
+    var song = songs[num].track;
+    console.log(song);
+    getSongData(song);
+}
+
+class GuessA extends React.Component {
+
+    constructor(props){
+      super(props);
+      this.test = this.test.bind(this);
+      this.submitAnswer = this.submitAnswer.bind(this);
+    }
+
+    submitAnswer(){
+        var userAnswer = document.getElementById("userAnswer").value.toUpperCase();
+        var correct = false;
+        for (var i = 0; i < artistsArr.length; i++){
+            if (artistsArr[i] == userAnswer){
+                correct = true;
+                break;
+            }
+        }
+        showAnswer(correct);
+    }
+
+    test(){
+        var result = document.getElementById("result").innerHTML = "";
+        var song = SpotifyClient.getSong(trackID);
+        var playlists = SpotifyClient.getFeaturedPlaylists();
+        playlists.then(playlistsSuccess, failure)
+        answered = false;
+        document.getElementById("submitAnswer").classList.remove("start");
+    }
+
+    state = { data : "Hello World" }
+
+
+    render() {
+        return (
+            <div class="centerGame">
+                <table class="guessArtist">
+                    <tbody>
+                        <tr>
+                            <th>
+                                <button onClick={this.test}>Give me a song!</button>
+                            </th>
+                            <th>
+                            </th>
+                        </tr>
+                        <tr>
+                            <td>
+                                <div id="title"></div>
+                            </td>
+                            <td rowspan="3">
+                                <div id="albumCover"></div>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <input type="text" id="userAnswer" />
+                                <br />
+                                <br />
+                                <button id="submitAnswer" class="start" onClick={this.submitAnswer}>Submit</button>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <div id="result"></div>
+                            </td>
+                        </tr>
+
+                    </tbody>
+                </table>
+            </div>
+
+        );
+    }
+}
+
+export default GuessA;
+
+//                <SongBlock dataFromParent = {this.state.data} />
--- frontend/gui/src/SpotifyClient.js
+++ frontend/gui/src/SpotifyClient.js
@@ -1,37 +1,38 @@
 import axios from 'axios'
 
-
-const SpotifyClient = {  
+const SpotifyClient = {
     client_id : '42b56bb8489a418db8bd7d2c631b2eef',
     user_token : null,
     loged_in : true,
-    refresh_token : "AQCKr3lnfNPGaGRv8sjoq5Yg2nC153VPaKGhG3ha4PQtWgWSJfHFiX13TSU57eu5XYa3RslbCbfckHN4c184_9FFh391FXEHZgVmXEU9pKOm2EkFy5DdLEvVL84vuvn6Gaw", 
+    refresh_token : "AQCKr3lnfNPGaGRv8sjoq5Yg2nC153VPaKGhG3ha4PQtWgWSJfHFiX13TSU57eu5XYa3RslbCbfckHN4c184_9FFh391FXEHZgVmXEU9pKOm2EkFy5DdLEvVL84vuvn6Gaw",
     base_url : 'https://api.spotify.com',
-    
+
 
     // api doc : https://developer.spotify.com/documentation/web-api/reference/
 
-    
+    //to read from a config file: https://stackoverflow.com/questions/38404816/reading-a-config-file
+
     GetAccessToken() {
         var _this = this;
+        var CONFIG = require('./client_secret.json');
+        var real_secret = CONFIG.secret;
 
         var secret = new XMLHttpRequest();
         secret.open('GET', './client_secret.config');
         secret.send();
         secret.onload = async function (){
+            if (real_secret){
 
-            if (secret.responseText){
-                
-                axios.post(  
-                    'https://accounts.spotify.com/api/token',  
-                        new URLSearchParams({ 
+                axios.post(
+                    'https://accounts.spotify.com/api/token',
+                        new URLSearchParams({
                             grant_type: "refresh_token",
-                            refresh_token :  _this.refresh_token, 
+                            refresh_token :  _this.refresh_token,
                         }).toString(),
                         {
                             headers: {
                                 'Content-Type': 'application/x-www-form-urlencoded',
-                                'Authorization': 'Basic ' + btoa(_this.client_id + ':' + secret.responseText)
+                                'Authorization': 'Basic ' + btoa(_this.client_id + ':' + real_secret)
                             },
                         }
                 ).then(function(response) {
@@ -42,14 +43,16 @@ const SpotifyClient = {
                     console.log(error);
 
                 })
-                
-                
+
+
             }
         }
     },
-    
+
     makeRequest(url_add){
         var _this = this;
+        _this.GetAccessToken();
+
 
         var resp =  axios.get(this.base_url + url_add, {
             headers : {
@@ -58,6 +61,7 @@ const SpotifyClient = {
         },)
         .catch(function(error) {
             if (error.response.status === "401"){
+                console.log("here");
                 _this.GetAccessToken();
             }
         })
@@ -66,8 +70,8 @@ const SpotifyClient = {
 
 
     //albums
-    async getAlbum(albumID){
-        return await this.makeRequest('/v1/albums/' + albumID);
+    async getAlbum(AlbumID){
+        return await this.makeRequest('/v1/albums/' + AlbumID);
     },
 
     async getAlbumTracks(albumID){
@@ -80,7 +84,7 @@ const SpotifyClient = {
     async getArtist(ArtistID){
         return await this.makeRequest('/v1/artists/' + ArtistID);
     },
-    
+
     async getArtistAlbums(ArtistID){
         return await this.makeRequest('/v1/artists/' + ArtistID +'/albums');
     },
@@ -94,7 +98,7 @@ const SpotifyClient = {
     // get userdata
     async getUserData() {
         return await this.makeRequest('/v1/me');
-    },   
+    },
 
     async getSong(songID) {
         return await this.makeRequest('/v1/tracks/'+songID);
@@ -109,6 +113,10 @@ const SpotifyClient = {
     // browse tab data
     async getCatagory(catagoryID) {
         return await this.makeRequest('/v1/browse/categories/' + catagoryID);
+    },
+
+    async getSongsOnPlaylist(playlistID) {
+        return await this.makeRequest('/v1/playlists/' + playlistID + '/tracks');
     },
 
     async getCatagoryPlaylist(catagoryID) {
