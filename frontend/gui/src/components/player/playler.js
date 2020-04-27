cf4923dfb9674cdb5f7451ab440f2f162f5033b8
Lenora Batcher <lenora.batcher@mymail.champlain.edu>
2020-04-27 10:12:24 -0400

    fixed player styling

--- frontend/gui/src/components/player/playler.js
+++ frontend/gui/src/components/player/playler.js
@@ -11,7 +11,7 @@ class Player extends React.Component {
             expandedPadding: "10000px",
             collapsed: true,
         }
-    } 
+    }
 
     handleClick = () => {
         if (this.state.collapsed) {
@@ -21,14 +21,14 @@ class Player extends React.Component {
             this.setState({collapsed: true})
         }
     }
-    
-    
-    
+
+
+
     render () {
         let collapsed = this.state.collapsed;
         let newHeight, newPadding, content;
         if (collapsed) {
-            let frame = <iframe title="player" id="1" width="90%" height={this.state.collapsedHeight} scrolling="auto" frameborder="no" allow="autoplay"
+            let frame = <iframe style={collpasedStyle} title="player" id="1" width="90%" height={this.state.collapsedHeight} scrolling="auto" frameborder="no" allow="autoplay"
                         src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/624958950&color=%2304ff71&auto_play=false&show_artwork=true&hide_related=false&show_comments=false&show_user=true&show_reposts=false&show_teaser=true&visual=false&buying=false&download=false">
                         </iframe>
             content =  <div style={{padding: '10px', width: '25%'}}>{frame}
@@ -36,10 +36,10 @@ class Player extends React.Component {
             </div>
         }
         else {
-            let frame = <iframe title="player" id="1" width="90%" height={this.state.expandedHeight} scrolling="auto" frameborder="no" allow="autoplay"
+            let frame = <iframe style={openStyle} title="player" id="1" width="90%" height={this.state.expandedHeight} scrolling="auto" frameborder="no" allow="autoplay"
                         src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/624958950&color=%2304ff71&auto_play=false&show_artwork=true&hide_related=false&show_comments=false&show_user=true&show_reposts=false&show_teaser=true&visual=false&buying=false&download=false">
                         </iframe>
-            content =  <div style={{paddingTop: '500px', width: '25%'}}>{frame}
+            content =  <div style={{paddingTop: '10px', width: '25%'}}>{frame}
             <button style={buttonStyle} onClick={this.handleClick}>↑</button>
             </div>
         }
@@ -52,6 +52,21 @@ const buttonStyle = {
     float: 'right',
     fontSize: '2vw',
     border: 'none'
+}
+
+const collpasedStyle = {
+    position: 'absolute',
+    top: '1px',
+    left: '20px',
+    'height': '5vw',
+    width: '20vw'
+}
+
+const openStyle = {
+    position: 'absolute',
+    top: '1px',
+    left: '20px',
+    width: '20vw'
 }
 
 export default Player;
